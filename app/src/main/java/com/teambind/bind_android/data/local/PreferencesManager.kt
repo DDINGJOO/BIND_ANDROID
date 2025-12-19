685d5a25648a858d5ab5f56c5629c5a3ade5a02f
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-19 15:05:36 +0900

    fix: PreferencesManager 수정

--- app/src/main/java/com/teambind/bind_android/data/local/PreferencesManager.kt
+++ app/src/main/java/com/teambind/bind_android/data/local/PreferencesManager.kt
@@ -2,11 +2,7 @@ package com.teambind.bind_android.data.local
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.longPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
