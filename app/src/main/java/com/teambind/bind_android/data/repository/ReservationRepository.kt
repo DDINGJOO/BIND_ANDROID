2af7f4f367ec19fb170ad69d46cb524701edcfe5
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-19 15:04:17 +0900

    feat: 예약 취소/환불 API 추가

--- app/src/main/java/com/teambind/bind_android/data/repository/ReservationRepository.kt
+++ app/src/main/java/com/teambind/bind_android/data/repository/ReservationRepository.kt
@@ -1,20 +1,8 @@
 package com.teambind.bind_android.data.repository
 
 import com.teambind.bind_android.data.api.service.ReservationService
-import com.teambind.bind_android.data.model.request.CancelReservationRequest
-import com.teambind.bind_android.data.model.request.CreateReservationRequest
-import com.teambind.bind_android.data.model.request.MultiSlotReservationRequest
-import com.teambind.bind_android.data.model.request.ProductQuantity
-import com.teambind.bind_android.data.model.request.ReservationPreviewRequest
-import com.teambind.bind_android.data.model.request.UpdateProductsRequest
-import com.teambind.bind_android.data.model.request.UpdateUserInfoRequest
-import com.teambind.bind_android.data.model.response.MyReservationDetailResponse
-import com.teambind.bind_android.data.model.response.MyReservationsResponse
-import com.teambind.bind_android.data.model.response.ReservationFieldDto
-import com.teambind.bind_android.data.model.response.ReservationPreviewResponse
-import com.teambind.bind_android.data.model.response.ReservationTimeResultResponse
-import com.teambind.bind_android.data.model.response.TimeSlotDto
-import com.teambind.bind_android.data.model.response.UpdatedReservationResponse
+import com.teambind.bind_android.data.model.request.*
+import com.teambind.bind_android.data.model.response.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
